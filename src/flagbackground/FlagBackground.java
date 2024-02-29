99efb2b4baba84000bf04cf999a74414ffb036ba
stone <samstone9001@gmail.com>
2024-02-29 15:13:36 -0500

    flag streches to edge

--- src/flagbackground/FlagBackground.java
+++ src/flagbackground/FlagBackground.java
@@ -1,41 +1,51 @@
-package flagbackground;
-
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
 public class FlagBackground {
 	
-	private static final int WIDTH = 1920;
-	private static final int HEIGHT = 1080;
-	private static final double SIZE = .4;
-	private static final int[] COLORS = {0xe9e162, 0xf6eeeb, 0x956db3, 0x373332};
+    private static final int WIDTH = 1920;
+    private static final int HEIGHT = 1080;
+    private static final double SIZE = .4;
 	
-	public static void main(String[] args) {
-		BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	    Graphics2D graphics = image.createGraphics();
-	    double flagHeight = HEIGHT * SIZE;
-	    int stripeHeight = (int) (flagHeight / COLORS.length);
-	    int start = (int) ((HEIGHT - flagHeight) / 2);
-	    for (int i = 0; i < COLORS.length; i++) {
-	    	for (int x = 0; x < WIDTH; x++) {
-	    		for (int y = 0; y < stripeHeight; y++) {
-	    			image.setRGB(x, start + y, COLORS[i]);
-	    		}
-	    	}
-	    	start += stripeHeight;
+    public static void main(String[] args) {
+	int[] COLORS = Arrays.stream(args).mapToInt((num) -> {return Integer.parseInt(num, 16);}).toArray();
+	BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	double flagHeight = HEIGHT * SIZE;
+	int stripeHeight = (int) (flagHeight / COLORS.length);
+	int start = (int) ((HEIGHT - flagHeight) / 2);
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < start; y++) {
+                image.setRGB(x, y, COLORS[0]);
+            }
+        }
+            
+	for (int i = 0; i < COLORS.length; i++) {
+	    for (int x = 0; x < WIDTH; x++) {
+		for (int y = 0; y < stripeHeight; y++) {
+		    image.setRGB(x, start + y, COLORS[i]);
+		}
 	    }
-	    
-	    try {
-	    	File file = new File("tmp.png");
-	    ImageIO.write(image, "png", file);
-	    }catch (IOException e) { 
-	       e.printStackTrace();
-	    }
+	    start += stripeHeight;
 	}
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = start; y < HEIGHT; y++) {
+                image.setRGB(x, y, COLORS[COLORS.length - 1]);
+            }
+        }
+	    
+	try {
+	    File file = new File("tmp.png");
+	    ImageIO.write(image, "png", file);
+	}catch (IOException e) { 
+	    e.printStackTrace();
+	}
+    }
 }
